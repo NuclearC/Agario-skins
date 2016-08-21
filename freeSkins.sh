49ef16900fcaf8e0b09fb356f2981a9ec7c10b8e
ItzLevvie <ItzLevvie@users.noreply.github.com>
2016-08-21 22:16:38 +0100

    Some fixes

--- freeSkins.sh
+++ freeSkins.sh
@@ -1,138 +1,75 @@
-wget configs-web.agario.miniclippt.com/live/v5/175/poland_hi.png && mv poland_hi.png poland.png
-wget configs-web.agario.miniclippt.com/live/v5/175/usa_hi.png && mv usa_hi.png usa.png
-wget configs-web.agario.miniclippt.com/live/v5/175/china_hi.png && mv china_hi.png china.png
-wget configs-web.agario.miniclippt.com/live/v5/175/russia_hi.png && mv russia_hi.png russia.png
-wget configs-web.agario.miniclippt.com/live/v5/175/canada_hi.png && mv canada_hi.png canada.png
-wget configs-web.agario.miniclippt.com/live/v5/175/australia_hi.png && mv australia_hi.png australia.png
-wget configs-web.agario.miniclippt.com/live/v5/175/spain_hi.png && mv spain_hi.png spain.png
-wget configs-web.agario.miniclippt.com/live/v5/175/brazil_hi.png && mv brazil_hi.png brazil.png
-wget configs-web.agario.miniclippt.com/live/v5/175/germany_hi.png && mv germany_hi.png germany.png
-wget configs-web.agario.miniclippt.com/live/v5/175/ukraine_hi.png && mv ukraine_hi.png ukraine.png
-wget configs-web.agario.miniclippt.com/live/v5/175/france_hi.png && mv france_hi.png france.png
-wget configs-web.agario.miniclippt.com/live/v5/175/sweden_hi.png && mv sweden_hi.png sweden.png
-wget configs-web.agario.miniclippt.com/live/v5/175/north_korea_hi.png && mv north_korea_hi.png north\ korea.png
-wget configs-web.agario.miniclippt.com/live/v5/175/south_korea_hi.png && mv south_korea_hi.png south\ korea.png
-wget configs-web.agario.miniclippt.com/live/v5/175/japan_hi.png && mv japan_hi.png japan.png
-wget configs-web.agario.miniclippt.com/live/v5/175/united_kingdom_hi.png && mv united_kingdom_hi.png united\ kingdom.png
-wget configs-web.agario.miniclippt.com/live/v5/175/earth_hi.png && mv earth_hi.png earth.png
-wget configs-web.agario.miniclippt.com/live/v5/175/greece_hi.png && mv greece_hi.png greece.png
-wget configs-web.agario.miniclippt.com/live/v5/175/latvia_hi.png && mv latvia_hi.png latvia.png
-wget configs-web.agario.miniclippt.com/live/v5/175/lithuania_hi.png && mv lithuania_hi.png lithuania.png
-wget configs-web.agario.miniclippt.com/live/v5/175/estonia_hi.png && mv estonia_hi.png estonia.png
-wget configs-web.agario.miniclippt.com/live/v5/175/finland_hi.png && mv finland_hi.png finland.png
-wget configs-web.agario.miniclippt.com/live/v5/175/norway_hi.png && mv norway_hi.png norway.png
-wget configs-web.agario.miniclippt.com/live/v5/175/cia_hi.png && mv cia_hi.png cia.png
-wget configs-web.agario.miniclippt.com/live/v5/175/brazil_hi.png && mv brazil_hi.png maldivas.png
-wget configs-web.agario.miniclippt.com/live/v5/175/austria_hi.png && mv austria_hi.png austria.png
-wget configs-web.agario.miniclippt.com/live/v5/175/nigeria_hi.png && mv nigeria_hi.png nigeria.png
-wget configs-web.agario.miniclippt.com/live/v5/175/reddit_hi.png && mv reddit_hi.png reddit.png
-wget configs-web.agario.miniclippt.com/live/v5/175/yaranaika_hi.png && mv yaranaika_hi.png yaranaika.png
-wget configs-web.agario.miniclippt.com/live/v5/175/9gag_hi.png && mv 9gag_hi.png 9gag.png
-wget configs-web.agario.miniclippt.com/live/v5/175/indiana_hi.png && mv indiana_hi.png indiana.png
-wget configs-web.agario.miniclippt.com/live/v5/175/4chan_hi.png && mv 4chan_hi.png 4chan.png
-wget configs-web.agario.miniclippt.com/live/v5/175/italy_hi.png && mv italy_hi.png italy.png
-wget configs-web.agario.miniclippt.com/live/v5/175/bulgaria_hi.png && mv bulgaria_hi.png bulgaria.png
-wget configs-web.agario.miniclippt.com/live/v5/175/2ch_hk_hi.png && mv 2ch_hk_hi.png 2ch.hk.png
-wget configs-web.agario.miniclippt.com/live/v5/175/hong_kong_hi.png && mv hong_kong_hi.png hong\ kong.png
-wget configs-web.agario.miniclippt.com/live/v5/175/portugal_hi.png && mv portugal_hi.png portugal.png
-wget configs-web.agario.miniclippt.com/live/v5/175/jamaica_hi.png && mv jamaica_hi.png jamaica.png
-wget configs-web.agario.miniclippt.com/live/v5/175/mexico_hi.png && mv mexico_hi.png mexico.png
-wget configs-web.agario.miniclippt.com/live/v5/175/sanik_hi.png && mv sanik_hi.png sanik.png
-wget configs-web.agario.miniclippt.com/live/v5/175/switzerland_hi.png && mv switzerland_hi.png switzerland.png
-wget configs-web.agario.miniclippt.com/live/v5/175/croatia_hi.png && mv croatia_hi.png croatia.png
-wget configs-web.agario.miniclippt.com/live/v5/175/chile_hi.png && mv chile_hi.png chile.png
-wget configs-web.agario.miniclippt.com/live/v5/175/indonesia_hi.png && mv indonesia_hi.png indonesia.png
-wget configs-web.agario.miniclippt.com/live/v5/175/bangladesh_hi.png && mv bangladesh_hi.png bangladesh.png
-wget configs-web.agario.miniclippt.com/live/v5/175/thailand_hi.png && mv thailand_hi.png thailand.png
-wget configs-web.agario.miniclippt.com/live/v5/175/iran_hi.png && mv iran_hi.png iran.png
-wget configs-web.agario.miniclippt.com/live/v5/175/iraq_hi.png && mv iraq_hi.png iraq.png
-wget configs-web.agario.miniclippt.com/live/v5/175/peru_hi.png && mv peru_hi.png peru.png
-wget configs-web.agario.miniclippt.com/live/v5/175/moon_hi.png && mv moon_hi.png moon.png
-wget configs-web.agario.miniclippt.com/live/v5/175/botswana_hi.png && mv botswana_hi.png botswana.png
-wget configs-web.agario.miniclippt.com/live/v5/175/bosnia_hi.png && mv bosnia_hi.png bosnia.png
-wget configs-web.agario.miniclippt.com/live/v5/175/netherlands_hi.png && mv netherlands_hi.png netherlands.png
-wget configs-web.agario.miniclippt.com/live/v5/175/european_union_hi.png && mv european_union_hi.png european\ union.png
-wget configs-web.agario.miniclippt.com/live/v5/175/taiwan_hi.png && mv taiwan_hi.png taiwan.png
-wget configs-web.agario.miniclippt.com/live/v5/175/pakistan_hi.png && mv pakistan_hi.png pakistan.png
-wget configs-web.agario.miniclippt.com/live/v5/175/hungary_hi.png && mv hungary_hi.png hungary.png
-wget configs-web.agario.miniclippt.com/live/v5/175/qing_dynasty_hi.png && mv qing_dynasty_hi.png qing\ dynasty.png
-wget configs-web.agario.miniclippt.com/live/v5/175/matriarchy_hi.png && mv matriarchy_hi.png matriarchy.png
-wget configs-web.agario.miniclippt.com/live/v5/175/patriarchy_hi.png && mv patriarchy_hi.png patriarchy.png
-wget configs-web.agario.miniclippt.com/live/v5/175/feminism_hi.png && mv feminism_hi.png feminism.png
-wget configs-web.agario.miniclippt.com/live/v5/175/ireland_hi.png && mv ireland_hi.png ireland.png
-wget configs-web.agario.miniclippt.com/live/v5/175/texas_hi.png && mv texas_hi.png texas.png
-wget configs-web.agario.miniclippt.com/live/v5/175/facepunch_hi.png && mv facepunch_hi.png facepunch.png
-wget configs-web.agario.miniclippt.com/live/v5/175/prodota_hi.png && mv prodota_hi.png prodota.png
-wget configs-web.agario.miniclippt.com/live/v5/175/cambodia_hi.png && mv cambodia_hi.png cambodia.png
-wget configs-web.agario.miniclippt.com/live/v5/175/steam_hi.png && mv steam_hi.png steam.png
-wget configs-web.agario.miniclippt.com/live/v5/175/piccolo_hi.png && mv piccolo_hi.png piccolo.png
-wget configs-web.agario.miniclippt.com/live/v5/175/ea_hi.png && mv ea_hi.png ea.png
-wget configs-web.agario.miniclippt.com/live/v5/175/india_hi.png && mv india_hi.png india.png
-wget configs-web.agario.miniclippt.com/live/v5/175/denmark_hi.png && mv denmark_hi.png denmark.png
-wget configs-web.agario.miniclippt.com/live/v5/175/quebec_hi.png && mv quebec_hi.png quebec.png
-wget configs-web.agario.miniclippt.com/live/v5/175/ayy_lmao_hi.png && mv ayy_lmao_hi.png ayy\ lmao.png
-wget configs-web.agario.miniclippt.com/live/v5/175/sealand_hi.png && mv sealand_hi.png sealand.png
-wget configs-web.agario.miniclippt.com/live/v5/175/bait_hi.png && mv bait_hi.png bait.png
-wget configs-web.agario.miniclippt.com/live/v5/175/origin_hi.png && mv origin_hi.png origin.png
-wget configs-web.agario.miniclippt.com/live/v5/175/vinesauce_hi.png && mv vinesauce_hi.png vinesauce.png
-wget configs-web.agario.miniclippt.com/live/v5/175/belgium_hi.png && mv belgium_hi.png belgium.png
-wget configs-web.agario.miniclippt.com/live/v5/175/luxembourg_hi.png && mv luxembourg_hi.png luxembourg.png
-wget configs-web.agario.miniclippt.com/live/v5/175/stussy_hi.png && mv stussy_hi.png stussy.png
-wget configs-web.agario.miniclippt.com/live/v5/175/8ch_hi.png && mv 8ch_hi.png 8ch.png
-wget configs-web.agario.miniclippt.com/live/v5/175/argentina_hi.png && mv argentina_hi.png argentina.png
-wget configs-web.agario.miniclippt.com/live/v5/175/scotland_hi.png && mv scotland_hi.png scotland.png
-wget configs-web.agario.miniclippt.com/live/v5/175/sir_hi.png && mv sir_hi.png sir.png
-wget configs-web.agario.miniclippt.com/live/v5/175/romania_hi.png && mv romania_hi.png romania.png
-wget configs-web.agario.miniclippt.com/live/v5/175/wojak_hi.png && mv wojak_hi.png wojak.png
-wget configs-web.agario.miniclippt.com/live/v5/175/doge_hi.png && mv doge_hi.png doge.png
-wget configs-web.agario.miniclippt.com/live/v5/175/nasa_hi.png && mv nasa_hi.png nasa.png
-wget configs-web.agario.miniclippt.com/live/v5/175/byzantium_hi.png && mv byzantium_hi.png byzantium.png
-wget configs-web.agario.miniclippt.com/live/v5/175/french_kingdom_hi.png && mv french_kingdom_hi.png french\ kingdom.png
-wget configs-web.agario.miniclippt.com/live/v5/175/somalia_hi.png && mv somalia_hi.png somalia.png
-wget configs-web.agario.miniclippt.com/live/v5/175/turkey_hi.png && mv turkey_hi.png turkey.png
-wget configs-web.agario.miniclippt.com/live/v5/175/mars_hi.png && mv mars_hi.png mars.png
-wget configs-web.agario.miniclippt.com/live/v5/175/pokerface_hi.png && mv pokerface_hi.png pokerface.png
-wget configs-web.agario.miniclippt.com/live/v5/175/irs_hi.png && mv irs_hi.png irs.png
-wget configs-web.agario.miniclippt.com/live/v5/175/receita_federal_hi.png && mv receita_federal_hi.png receita\ federal.png
-wget configs-web.agario.miniclippt.com/live/v5/175/8_hi.png && mv 8_hi.png 8.png
-wget configs-web.agario.miniclippt.com/live/v5/175/tsipras_hi.png && mv tsipras_hi.png tsipras.png
-wget configs-web.agario.miniclippt.com/live/v5/175/berlusconi_hi.png && mv berlusconi_hi.png berlusconi.png
-wget configs-web.agario.miniclippt.com/live/v5/175/blatter_hi.png && mv blatter_hi.png blatter.png
-wget configs-web.agario.miniclippt.com/live/v5/175/boris_hi.png && mv boris_hi.png boris.png
-wget configs-web.agario.miniclippt.com/live/v5/175/bush_hi.png && mv bush_hi.png bush.png
-wget configs-web.agario.miniclippt.com/live/v5/175/cameron_hi.png && mv cameron_hi.png cameron.png
-wget configs-web.agario.miniclippt.com/live/v5/175/chavez_hi.png && mv chavez_hi.png chavez.png
-wget configs-web.agario.miniclippt.com/live/v5/175/clinton_hi.png && mv clinton_hi.png clinton.png
-wget configs-web.agario.miniclippt.com/live/v5/175/cuba_hi.png && mv cuba_hi.png cuba.png
-wget configs-web.agario.miniclippt.com/live/v5/175/dilma_hi.png && mv dilma_hi.png dilma.png
-wget configs-web.agario.miniclippt.com/live/v5/175/fidel_hi.png && mv fidel_hi.png fidel.png
-wget configs-web.agario.miniclippt.com/live/v5/175/hillary_hi.png && mv hillary_hi.png hillary.png
-wget configs-web.agario.miniclippt.com/live/v5/175/hollande_hi.png && mv hollande_hi.png hollande.png
-wget configs-web.agario.miniclippt.com/live/v5/175/merkel_hi.png && mv merkel_hi.png merkel.png
-wget configs-web.agario.miniclippt.com/live/v5/175/obama_hi.png && mv obama_hi.png obama.png
-wget configs-web.agario.miniclippt.com/live/v5/175/palin_hi.png && mv palin_hi.png palin.png
-wget configs-web.agario.miniclippt.com/live/v5/175/putin_hi.png && mv putin_hi.png putin.png
-wget configs-web.agario.miniclippt.com/live/v5/175/queen_hi.png && mv queen_hi.png queen.png
-wget configs-web.agario.miniclippt.com/live/v5/175/trump_hi.png && mv trump_hi.png trump.png
-wget configs-web.agario.miniclippt.com/live/v5/175/venezuela_hi.png && mv venezuela_hi.png venezuela.png
-wget configs-web.agario.miniclippt.com/live/v5/175/Colombia_hi.png && mv Colombia_hi.png colombia.png
-wget configs-web.agario.miniclippt.com/live/v5/175/Soccer_hi.png && mv Soccer_hi.png football.png
-wget configs-web.agario.miniclippt.com/live/v5/175/varoufakis_hi.png && mv varoufakis_hi.png varoufakis.png
-wget configs-web.agario.miniclippt.com/live/v5/175/House_of_Cards_hi.png && mv House_of_Cards_hi.png underwood.png
-wget configs-web.agario.miniclippt.com/live/v5/175/chaplin_hi.png && mv chaplin_hi.png chaplin.png
-wget configs-web.agario.miniclippt.com/live/v5/175/confederate_hi.png && mv confederate_hi.png confederate.png
-wget configs-web.agario.miniclippt.com/live/v5/175/tumblr_hi.png && mv tumblr_hi.png tumblr.png
-wget configs-web.agario.miniclippt.com/live/v5/175/german_empire_hi.png && mv german_empire_hi.png german\ empire.png
-wget configs-web.agario.miniclippt.com/live/v5/175/satanist_hi.png && mv satanist_hi.png satanist.png
-wget configs-web.agario.miniclippt.com/live/v5/175/kc_hi.png && mv kc_hi.png kc.png
-wget configs-web.agario.miniclippt.com/live/v5/175/tsarist_russia_hi.png && mv tsarist_russia_hi.png tsarist\ russia.png
-wget configs-web.agario.miniclippt.com/live/v5/175/stalin_hi.png && mv stalin_hi.png stalin.png
-wget configs-web.agario.miniclippt.com/live/v5/175/prussia_hi.png && mv prussia_hi.png prussia.png
-wget configs-web.agario.miniclippt.com/live/v5/175/imperial_japan_hi.png && mv imperial_japan_hi.png imperial\ japan.png
-wget configs-web.agario.miniclippt.com/live/v5/175/facebook_hi.png && mv facebook_hi.png facebook.png
-wget configs-web.agario.miniclippt.com/live/v5/175/kim_jong_un_hi.png && mv kim_jong_un_hi.png kim\ jong-un.png
-wget configs-web.agario.miniclippt.com/live/v5/175/Sports_Flame_hi.png && mv Sports_Flame_hi.png agario.png
-wget configs-web.agario.miniclippt.com/live/v5/175/Yellow_hi.png && mv Yellow_hi.png Pika.png
-wget configs-web.agario.miniclippt.com/live/v5/175/Blue_hi.png && mv Blue_hi.png Squi.png
-wget configs-web.agario.miniclippt.com/live/v5/175/Green_hi.png && mv Green_hi.png Bulba.png
-wget configs-web.agario.miniclippt.com/live/v5/175/Red_hi.png && mv Red_hi.png Char.png
+wget configs-web.agario.miniclippt.com/live/v5/175/poland_hi.png -O poland.png
+wget configs-web.agario.miniclippt.com/live/v5/175/usa_hi.png -O usa.png
+wget configs-web.agario.miniclippt.com/live/v5/175/china_hi.png -O china.png
+wget configs-web.agario.miniclippt.com/live/v5/175/russia_hi.png -O russia.png
+wget configs-web.agario.miniclippt.com/live/v5/175/canada_hi.png -O canada.png
+wget configs-web.agario.miniclippt.com/live/v5/175/australia_hi.png -O australia.png
+wget configs-web.agario.miniclippt.com/live/v5/175/spain_hi.png -O spain.png
+wget configs-web.agario.miniclippt.com/live/v5/175/brazil_hi.png -O brazil.png
+wget configs-web.agario.miniclippt.com/live/v5/175/germany_hi.png -O germany.png
+wget configs-web.agario.miniclippt.com/live/v5/175/ukraine_hi.png -O ukraine.png
+wget configs-web.agario.miniclippt.com/live/v5/175/france_hi.png -O france.png
+wget configs-web.agario.miniclippt.com/live/v5/175/sweden_hi.png -O sweden.png
+wget configs-web.agario.miniclippt.com/live/v5/175/north_korea_hi.png -O north\ korea.png
+wget configs-web.agario.miniclippt.com/live/v5/175/south_korea_hi.png -O south\ korea.png
+wget configs-web.agario.miniclippt.com/live/v5/175/japan_hi.png -O japan.png
+wget configs-web.agario.miniclippt.com/live/v5/175/united_kingdom_hi.png -O united\ kingdom.png
+wget configs-web.agario.miniclippt.com/live/v5/175/earth_hi.png -O earth.png
+wget configs-web.agario.miniclippt.com/live/v5/175/greece_hi.png -O greece.png
+wget configs-web.agario.miniclippt.com/live/v5/175/latvia_hi.png -O latvia.png
+wget configs-web.agario.miniclippt.com/live/v5/175/lithuania_hi.png -O lithuania.png
+wget configs-web.agario.miniclippt.com/live/v5/175/estonia_hi.png -O estonia.png
+wget configs-web.agario.miniclippt.com/live/v5/175/finland_hi.png -O finland.png
+wget configs-web.agario.miniclippt.com/live/v5/175/norway_hi.png -O norway.png
+wget configs-web.agario.miniclippt.com/live/v5/175/cia_hi.png -O cia.png
+wget configs-web.agario.miniclippt.com/live/v5/175/brazil_hi.png -O maldivas.png
+wget configs-web.agario.miniclippt.com/live/v5/175/austria_hi.png -O austria.png
+wget configs-web.agario.miniclippt.com/live/v5/175/nigeria_hi.png -O nigeria.png
+wget configs-web.agario.miniclippt.com/live/v5/175/reddit_hi.png -O reddit.png
+wget configs-web.agario.miniclippt.com/live/v5/175/yaranaika_hi.png -O yaranaika.png
+wget configs-web.agario.miniclippt.com/live/v5/175/9gag_hi.png -O 9gag.png
+wget configs-web.agario.miniclippt.com/live/v5/175/indiana_hi.png -O indiana.png
+wget configs-web.agario.miniclippt.com/live/v5/175/4chan_hi.png -O 4chan.png
+wget configs-web.agario.miniclippt.com/live/v5/175/italy_hi.png -O italy.png
+wget configs-web.agario.miniclippt.com/live/v5/175/bulgaria_hi.png -O bulgaria.png
+wget configs-web.agario.miniclippt.com/live/v5/175/2ch_hk_hi.png -O 2ch.hk.png
+wget configs-web.agario.miniclippt.com/live/v5/175/hong_kong_hi.png -O hong\ kong.png
+wget configs-web.agario.miniclippt.com/live/v5/175/portugal_hi.png -O portugal.png
+wget configs-web.agario.miniclippt.com/live/v5/175/jamaica_hi.png -O jamaica.png
+wget configs-web.agario.miniclippt.com/live/v5/175/mexico_hi.png -O mexico.png
+wget configs-web.agario.miniclippt.com/live/v5/175/sanik_hi.png -O sanik.png
+wget configs-web.agario.miniclippt.com/live/v5/175/switzerland_hi.png -O switzerland.png
+wget configs-web.agario.miniclippt.com/live/v5/175/croatia_hi.png -O croatia.png
+wget configs-web.agario.miniclippt.com/live/v5/175/chile_hi.png -O chile.png
+wget configs-web.agario.miniclippt.com/live/v5/175/indonesia_hi.png -O indonesia.png
+wget configs-web.agario.miniclippt.com/live/v5/175/bangladesh_hi.png -O bangladesh.png
+wget configs-web.agario.miniclippt.com/live/v5/175/thailand_hi.png -O thailand.png
+wget configs-web.agario.miniclippt.com/live/v5/175/iran_hi.png -O iran.png
+wget configs-web.agario.miniclippt.com/live/v5/175/iraq_hi.png -O iraq.png
+wget configs-web.agario.miniclippt.com/live/v5/175/peru_hi.png -O peru.png
+wget configs-web.agario.miniclippt.com/live/v5/175/moon_hi.png -O moon.png
+wget configs-web.agario.miniclippt.com/live/v5/175/botswana_hi.png -O botswana.png
+wget configs-web.agario.miniclippt.com/live/v5/175/bosnia_hi.png -O bosnia.png
+wget configs-web.agario.miniclippt.com/live/v5/175/netherlands_hi.png -O netherlands.png
+wget configs-web.agario.miniclippt.com/live/v5/175/european_union_hi.png -O european\ union.png
+wget configs-web.agario.miniclippt.com/live/v5/175/taiwan_hi.png -O taiwan.png
+wget configs-web.agario.miniclippt.com/live/v5/175/pakistan_hi.png -O pakistan.png
+wget configs-web.agario.miniclippt.com/live/v5/175/hungary_hi.png -O hungary.png
+wget configs-web.agario.miniclippt.com/live/v5/175/qing_dynasty_hi.png -O qing\ dynasty.png
+wget configs-web.agario.miniclippt.com/live/v5/175/matriarchy_hi.png -O matriarchy.png
+wget configs-web.agario.miniclippt.com/live/v5/175/patriarchy_hi.png -O patriarchy.png
+wget configs-web.agario.miniclippt.com/live/v5/175/feminism_hi.png -O feminism.png
+wget configs-web.agario.miniclippt.com/live/v5/175/ireland_hi.png -O ireland.png
+wget configs-web.agario.miniclippt.com/live/v5/175/texas_hi.png -O texas.png
+wget configs-web.agario.miniclippt.com/live/v5/175/facepunch_hi.png -O facepunch.png
+wget configs-web.agario.miniclippt.com/live/v5/175/prodota_hi.png -O prodota.png
+wget configs-web.agario.miniclippt.com/live/v5/175/cambodia_hi.png -O cambodia.png
+wget configs-web.agario.miniclippt.com/live/v5/175/steam_hi.png -O steam.png
+wget configs-web.agario.miniclippt.com/live/v5/175/piccolo_hi.png -O piccolo.png
+wget configs-web.agario.miniclippt.com/live/v5/175/ea_hi.png -O ea.png
+wget configs-web.agario.miniclippt.com/live/v5/175/india_hi.png -O india.png
+wget configs-web.agario.miniclippt.com/live/v5/175/denmark_hi.png -O denmark.png
+wget configs-web.agario.miniclippt.com/live/v5/175/quebec_hi.png -O quebec.png
+wget configs-web.agario.miniclippt.com/live/v5/175/ayy_lmao_hi.png -O ayy\ lmao.png
+wget configs-web.agario.miniclippt.com/live/v5/175/sealand_hi.png -O sealand.png
+wget configs-web.agario.miniclippt.com/live/v5/175/bait_hi.png -O bait.png
